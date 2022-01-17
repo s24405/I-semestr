65fe970e750fb1fce1ace36d573cfbc8becfb875
s24405 <91895791+s24405@users.noreply.github.com>
2022-01-17 11:52:01 +0100

    Update prg2zad6.cpp

--- prg2zad6.cpp
+++ prg2zad6.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <string>
-using namespace std;
 
 struct Student{
 
-    string imie;
-    string nazwisko;
-    vector<double>oceny;
+    std::string name;
+    std::string surname;
+    std::vector<double> grades;
 
 };
 
 
-Student najlepszy_student(vector<Student> studs)
-{
-    vector<double> srednia;
-int i,j;
-    for(int i = studs){
-        double srednia;
-        for( auto j : i.oceny){
-            srednia += j;
+Student get_best_student(std::vector<Student> studs){
+    std::vector<double> averages;
+
+    for(auto i : studs){
+        double average;
+        for( auto j : i.grades){
+            average += j;
         }
-    srednia/= i.oceny.size();
-    srednia.push_back(srednia);
+    average/= i.grades.size();
+    averages.push_back(average);
     }
     double max;
     int stdn;
     
-    for(int i=0; i<srednia.size(); i++){
-        if(srednia[i]>max){
-            max=srednia[i];
+    for(int i=0; i<averages.size(); i++){
+        if(averages[i]>max){
+            max=averages[i];
             stdn=i;
         }
     }
@@ -37,11 +35,11 @@ int i,j;
 }
 
 int main(){
-    auto s1 = Student{"Mila", "Jankowska", {6,5.6,6,2,5,4,3}};
-    auto s2 = Student{"Karolina", "Bielecka" ,{2,3,1.5,4,4,2}};
-    auto s3 = Student{"Kinga", "lukowska", {5,4,4,1,1,1.5,6}};
-    auto s4 = Student{"Janina", "Szybinska",{1,2.4,3,4,1,5,2}};
-    vector<Student> students = {s1, s2, s3, s4};
-	cout<<"najlepszy student: "<< najlepszy_student(students).imie<<endl;
+    auto s1 = Student{"Kinga", "Kolowska", {6,5,2,2,1,4,1}};
+    auto s2 = Student{"Marta", "Bielik" ,{2,6,5.5,1,1,2}};
+    auto s3 = Student{"Iza", "Kiedrowska", {5,6,6,6,3,1.5,2.5}};
+    auto s4 = Student{"Kuba", "Hinc",{1,2,2,1,1,5.5,5.5}};
+    std::vector<Student> students = {s1, s2, s3, s4};
+    std::cout<<"Best student: "<< get_best_student(students).name<<"\n";
     return 0;
 }
