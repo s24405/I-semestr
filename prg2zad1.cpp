6059d4f00abd77cf6be8eb451349b44f9f7b3391
s24405 <91895791+s24405@users.noreply.github.com>
2022-01-17 11:46:41 +0100

    Update prg2zad1.cpp

--- prg2zad1.cpp
+++ prg2zad1.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
-auto average_of(vector<float> oceny )->float
-{
-	int suma=0;
-	for (int x=0; oceny.size()>x; x++)
-	{
-		suma+=oceny[x];
-	}
-	float average=suma/oceny.sixe();
-	return average;
+#include <cmath>
+#include <algorithm>
+float average_of(std::vector<double> vec){
+    float suma=0;    
+    for(int i=0; i<vec.size(); i++){
+        suma+=vec[i];
+    }
+    float srednia = suma/vec.size();
+    return srednia;
 }
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	vector<float> oceny{5,3,2,6,1};
-	
-	cout<<average_of(oceny);
-	return 0;
+
+int main(){
+    auto numbers = std::vector<double>{1,2,2.5,8,5,4,9};
+    std::cout<<average_of(numbers)<<"\n";
+    return 0;
 }
