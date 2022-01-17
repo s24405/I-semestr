fb1438ff60d95584c215352a7e7b23311cbd0793
s24405 <91895791+s24405@users.noreply.github.com>
2022-01-17 11:36:44 +0100

    Update zadanie6.cpp

--- zadanie6.cpp
+++ zadanie6.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	int a;
-	cout<<"podaj wartosc"<<endl;
-	cin>>a;
-	Even(a)
-cout<<a;
-	return 0;
+bool is_divisible(int x,int y){
+    if(x%y==0)
+        return true;
+    return false;
+
+}
+bool is_even(int x){
+    if(is_divisible(x,2))
+        return true;
+    return false;
+    
+}
+int main(){
+    int x;
+    std::cout<<"Podaj liczbe do sprawdzenia : ";
+    std::cin>>x;
+    if(is_even(x)) std::cout<<"Parzysta\n";
+    else std::cout<<"Nieparzysta\n";
+    return 0;
 }
